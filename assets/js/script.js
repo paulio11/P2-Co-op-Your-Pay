0692549ebab01e10fd288f60029954fec54e0547
Paul Young <youngjpj@gmail.com>
2022-08-19 17:17:23 +0000

    Add if statement to output correct dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,56 @@ function populateCalendar() {
   let payday = '';
   let nextPayday = '';
 
-  
+  if (payPeriodValue == '202201') {
+    selectedPeriod = period202201;
+    weekNumbers = [51, 52, 1, 2];
+    payday = '13th January';
+    nextPayday = '10th February';
+  } else if (payPeriodValue == '202202') {
+    selectedPeriod = period202202;
+    weekNumbers = [3, 4, 5, 6];
+    payday = '10th February';
+    nextPayday = '10th March';
+  } else if (payPeriodValue == '202203') {
+    selectedPeriod = period202203;
+    weekNumbers = [7, 8, 9, 10];
+    payday = '10th March';
+    nextPayday = '7th April';
+  } else if (payPeriodValue == '202204') {
+    selectedPeriod = period202204;
+    weekNumbers = [11, 12, 13, 14];
+    payday = '7th April';
+    nextPayday = '5th May';
+  } else if (payPeriodValue == '202205') {
+    selectedPeriod = period202205;
+    weekNumbers = [15, 16, 17, 18];
+    payday = '5th May';
+    nextPayday = '1st June';
+  } else if (payPeriodValue == '202206') {
+    selectedPeriod = period202206;
+    weekNumbers = [19, 20, 21, 22];
+    payday = '1st June';
+    nextPayday = '30th June';
+  } else if (payPeriodValue == '202207') {
+    selectedPeriod = period202207;
+    weekNumbers = [23, 24, 25, 26];
+    payday = '30th June';
+    nextPayday = '28th July';
+  } else if (payPeriodValue == '202208') {
+    selectedPeriod = period202208;
+    weekNumbers = [27, 28, 29, 30];
+    payday = '28th July';
+    nextPayday = '25th August';
+  } else if (payPeriodValue == '202209') {
+    selectedPeriod = period202209;
+    weekNumbers = [31, 32, 33, 34];
+    payday = '25th August';
+    nextPayday = '22nd September';
+  } else if (payPeriodValue == '220210') {
+    selectedPeriod = period202210;
+    weekNumbers = [35, 36, 37, 38];
+    payday = '22nd September';
+    nextPayday = '20th October';
+  }
 
 }
