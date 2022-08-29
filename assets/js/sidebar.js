2c8c3ba4e3d2ae8c54214b0b14fa1f99ba47cb85
Paul Young <youngjpj@gmail.com>
2022-08-29 22:55:52 +0000

    Add style and script to animate sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -9,14 +9,20 @@ let sidebarMenu = document.getElementById('sidebar');
 
 function openSidebar() {  
 
-  sidebarContainer.style.display = 'block';
+  sidebarContainer.style.top = '0';
+  sidebarContainer.style.left = '0';
   document.body.style.overflow = 'hidden';
+  sidebarMenu.classList.add('sidebar-open');
+  sidebarMenu.classList.remove('sidebar-closed');
 
 }
 
 function closeSidebar() {
   
-  sidebarContainer.style.display = 'none';
+  sidebarContainer.style.top = '-100vh';
+  sidebarContainer.style.left = '-100vw';
   document.body.style.overflow = 'initial';
+  sidebarMenu.classList.add('sidebar-closed');
+  sidebarMenu.classList.remove('sidebar-open');
 
 }
