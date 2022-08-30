e2479e76d36abab7ab7cd5a3c454938d22aa0ecb
Paul Young <youngjpj@gmail.com>
2022-08-30 15:16:26 +0000

    Fix function order

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -18,8 +18,8 @@ function openSidebar() {
   sidebarContainer.style.top = '0';
   sidebarContainer.style.left = '0';
   document.body.style.overflow = 'hidden';
-  sidebarMenu.classList.add('sidebar-open');
   sidebarMenu.classList.remove('sidebar-closed');
+  sidebarMenu.classList.add('sidebar-open');
 
 }
 
@@ -31,8 +31,8 @@ function closeSidebar() {
   sidebarContainer.style.top = '-100vh';
   sidebarContainer.style.left = '-100vw';
   document.body.style.overflow = 'initial';
-  sidebarMenu.classList.add('sidebar-closed');
   sidebarMenu.classList.remove('sidebar-open');
+  sidebarMenu.classList.add('sidebar-closed');
 
 }
 
