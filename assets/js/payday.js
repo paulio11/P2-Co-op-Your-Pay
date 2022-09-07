44c5baaad71fc5dc7476fbd059185b2802714fad
Paul Young <youngjpj@gmail.com>
2022-09-07 15:21:38 +0000

    Add array paydays, and for loop to find next date

--- assets/js/payday.js
+++ assets/js/payday.js
@@ -7,4 +7,16 @@ const options = {
   day: 'numeric'
 };
 
+//remember jan = 0
+const paydays = [new Date(2022, 8, 22), new Date(2022, 9, 20), new Date(2022, 10, 17), new Date(2022, 11, 15)];
+
+let nextPayday = null;
+for (let i = 0; i < paydays.length; i++) {
+  if (paydays[i] > todayDate) {
+    nextPayday = paydays[i];
+    break;
+  }
+}
+
 document.getElementById('today').innerHTML = todayDate.toLocaleString('en-GB', options);
+document.getElementById('next-payday').innerHTML = nextPayday.toLocaleString('en-GB', options);
