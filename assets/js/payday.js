03bcda01f180f0da375ca82b7ad99f5a24a28924
Paul Young <youngjpj@gmail.com>
2022-09-07 14:11:33 +0000

    Add toLocaleString to display date in span

--- assets/js/payday.js
+++ assets/js/payday.js
@@ -1,21 +1,10 @@
-const today = new Date();
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let month = months[today.getMonth()];
+const todayDate = new Date();
 
-//looks at last digit of date number and adds correct suffix to the string.
-const dateSuffix = (date) => {
-  var dateString = date.toString();
-  var lastDigit = dateString.slice(-1);
-  var secondLast = dateString.length > 1 ? dateString.slice(-2, -1) : null;
-  //for dates 11th, 12th, and 13th.
-  if (secondLast && secondLast == '1') return 'th';
-  if (lastDigit == '1') return 'st';
-  if (lastDigit == '2') return 'nd';
-  if (lastDigit == '3') return 'rd';
-  return 'th';
-}
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
 
-
-document.getElementById('day').innerHTML = today.getDate() + dateSuffix(today.getDate());
-document.getElementById('month').innerHTML = month;
-document.getElementById('year').innerHTML = today.getFullYear();
+document.getElementById('today').innerHTML = todayDate.toLocaleString('en-GB', options);
