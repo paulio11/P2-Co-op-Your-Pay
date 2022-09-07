4b4aa00ca206ad8487289ee74b5fd653f7f0d583
Paul Young <youngjpj@gmail.com>
2022-09-07 15:39:04 +0000

    Add variable to calculate days until next payday

--- assets/js/payday.js
+++ assets/js/payday.js
@@ -1,14 +1,11 @@
 const todayDate = new Date();
-
 const options = {
   weekday: 'long',
   year: 'numeric',
   month: 'long',
   day: 'numeric'
 };
-
-//remember jan = 0
-const paydays = [new Date(2022, 8, 22), new Date(2022, 9, 20), new Date(2022, 10, 17), new Date(2022, 11, 15)];
+const paydays = [new Date(2022, 8, 22), new Date(2022, 9, 20), new Date(2022, 10, 17), new Date(2022, 11, 15)]; //remember jan = 0
 
 let nextPayday = null;
 for (let i = 0; i < paydays.length; i++) {
@@ -18,5 +15,8 @@ for (let i = 0; i < paydays.length; i++) {
   }
 }
 
-document.getElementById('today').innerHTML = todayDate.toLocaleString('en-GB', options);
+let daysUntil = nextPayday - todayDate;
+
+// document.getElementById('today').innerHTML = todayDate.toLocaleString('en-GB', options);
 document.getElementById('next-payday').innerHTML = nextPayday.toLocaleString('en-GB', options);
+document.getElementById('days-until').innerHTML = (daysUntil / (1000 * 3600 * 24)).toFixed(0);
