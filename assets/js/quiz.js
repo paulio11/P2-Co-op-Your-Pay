de01e7a17f927a98cc78789ab2188cfe10a55f3b
Paul Young <youngjpj@gmail.com>
2022-09-15 16:31:08 +0000

    Define variables

---
+++ assets/js/quiz.js
@@ -0,0 +1,11 @@
+const questionDiv = document.getElementById('question');
+const answersDiv = document.getElementById('answers');
+const buttonsDiv = document.getElementById('quiz-buttons');
+const scoreDiv = document.getElementById('score');
+const nextButton = document.getElementById('next-button');
+const quizContainer = document.getElementById('quiz-container');
+const quizTracker = document.getElementById('quiz-tracker');
+const startButton = document.getElementById('start-button');
+
+let number = 0;
+let score = 0;
