4dc026bee47defe57802105d9f6611ff6399ca94
Paul Young <youngjpj@gmail.com>
2022-09-15 16:22:23 +0000

    Initial commit - quiz questions array

---
+++ assets/js/questions.js
@@ -0,0 +1,78 @@
+const questions = [{
+    number: 1,
+    questionText: 'How often do you get paid as a Co-op Employee?',
+    options: [
+      'Weekly',
+      'Every four weeks',
+      'Monthly',
+      'Yearly'
+    ],
+    answer: 'Every four weeks'
+  },
+  {
+    number: 2,
+    questionText: 'What do of the week do you get paid on?',
+    options: [
+      'Monday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ],
+    answer: 'Thursday'
+  },
+  {
+    number: 3,
+    questionText: 'Which of the following payslip deductions represents your pension contribution',
+    options: [
+      'PACE Salary Sacrifice',
+      'Student Loan',
+      'NI Employee',
+      'TS Punctuality'
+    ],
+    answer: 'PACE Salary Sacrifice'
+  },
+  {
+    number: 4,
+    questionText: 'Which of the following payslip payments represents the ammount you have been paid after all deductions?',
+    options: [
+      'Annual Salary',
+      'Net Pay',
+      'Total Earnings',
+      'SSP'
+    ],
+    answer: 'Net Pay'
+  },
+  {
+    number: 5,
+    questionText: 'Which website do you use to see your payslip?',
+    options: [
+      'How Do I',
+      'Shifts',
+      'MyHR',
+      'GOV.uk'
+    ],
+    answer: 'MyHR'
+  },
+  {
+    number: 6,
+    questionText: 'Which of the following is correct?',
+    options: [
+      'Each pay period lasts one month',
+      'All overtime earned in a pay period is paid on the next payday',
+      'Your boss always gets a longer break than you',
+      "Paul's payday calculator is awesome"
+    ],
+    answer: "Paul's payday calculator is awesome"
+  },
+  {
+    number: 7,
+    questionText: 'You have worked an 8 hour shift - what breaks are you entitled to?',
+    options: [
+      'No breaks',
+      'A 1 hour paid break',
+      'Two 15 minute unpaid breaks',
+      'Two 15 minute paid breajs and a 30 minute unpaid break'
+    ],
+    answer: 'Two 15 minute paid breajs and a 30 minute unpaid break'
+  }
+]
